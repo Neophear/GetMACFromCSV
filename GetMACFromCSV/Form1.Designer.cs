8294fa93f76346afb8ea083e36c189462d4cdc6c
Neophear <stiiggade@gmail.com>
2019-10-11 10:03:27 +0200

    Initial commit

---
+++ GetMACFromCSV/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace GetMACFromCSV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.ofdFile = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.AllowDrop = true;
+            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutput.Location = new System.Drawing.Point(13, 42);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput.Size = new System.Drawing.Size(259, 208);
+            this.txtOutput.TabIndex = 0;
+            this.txtOutput.DragEnter += new System.Windows.Forms.DragEventHandler(this.txtOutput_DragEnter);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(13, 13);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(55, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Åben";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // ofdFile
+            // 
+            this.ofdFile.FileName = "*.csv";
+            this.ofdFile.Filter = "CSV-files|*.csv|All files|*.*";
+            this.ofdFile.FileOk += new System.ComponentModel.CancelEventHandler(this.ofdFile_FileOk);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.txtOutput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.OpenFileDialog ofdFile;
+
+    }
+}
+
---
+++ GetMACFromCSV/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GetMACFromCSV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtOutput_DragEnter(object sender, DragEventArgs e)
+        {
+            
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            ofdFile.ShowDialog();
+        }
+
+        private void ofdFile_FileOk(object sender, CancelEventArgs e)
+        {
+            if (!ofdFile.CheckFileExists)
+            {
+                MessageBox.Show("Filen eksisterer ikke");
+            }
+            else
+            {
+                foreach (string line in File.ReadAllLines(ofdFile.FileName))
+                {
+                    string[] parts = line.Split(',');
+                    Regex validMac = new Regex("^[a-fA-F0-9]{12}$");
+                    string MAC = parts[4];
+
+                    if (validMac.IsMatch(MAC))
+                        txtOutput.AppendText(ConvertStringToMAC(MAC) + Environment.NewLine);
+                }
+            }
+        }
+
+        private string ConvertStringToMAC(string text)
+        {
+            return String.Format("{0}:{1}:{2}:{3}:{4}:{5}", text.Substring(0, 2), text.Substring(2, 2), text.Substring(4, 2), text.Substring(6, 2), text.Substring(8, 2), text.Substring(10,2));
+        }
+    }
+}
